51ed0d8b8423fd3b122ae18f0fc2002957d8e2f4
Kevin GANDOLFI <kevin.gandolfi@alten.com>
2025-08-04 15:14:18 +0200

    show hidden files with snacks explorer

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,23 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      enabled = true,
+      hidden = true,
+      ignored = true,
+    },
+    picker = {
+      enabled = true,
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+        files = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
+  },
+}
